9fea098e08bc6630aaacc4042d13ca088a17adbb
Ahmet Furkan Meric <afm634@gmail.com>
2024-04-22 13:58:00 +0300

    Create singlePage.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import HomePage from "./routes/HomePage/homePage";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import SinglePage from "./routes/singlePage/singlePage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/routes/HomePage/homePage.jsx
@@ -0,0 +1,40 @@
+import "./homePage.scss";
+import SearchBar from "../../components/searchBar/SearchBar";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut maiores
+            aspernatur, placeat, animi suscipit laborum distinctio itaque
+            adipisci blanditiis, cumque saepe repellendus quae soluta?
+            Temporibus officiis mollitia harum id sed!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years Of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
